bdbef54b685feeac9bfd4ab3cf2f4fad4408cdaf
Pawel Martuszewski <pawel.martuszewski@gmail.com>
2016-07-28 22:40:25 +0100

    remove simple project as we dont need it really

--- www/fms/app/config/config.php
+++ www/fms/app/config/config.php
@@ -1,24 +1,17 @@
 <?php
 
-defined('APP_PATH') || define('APP_PATH', realpath('.'));
-
 return new \Phalcon\Config(array(
-    'database' => array(
-        'adapter'     => 'Sqlite',
-        'host'        => 'localhost',
-        'username'    => 'root',
-        'password'    => '',
-        'dbname'      => 'test',
-        'charset'     => 'utf8',
-    ),
-    'application' => array(
-        'controllersDir' => APP_PATH . '/app/controllers/',
-        'modelsDir'      => APP_PATH . '/app/models/',
-        'migrationsDir'  => APP_PATH . '/app/migrations/',
-        'viewsDir'       => APP_PATH . '/app/views/',
-        'pluginsDir'     => APP_PATH . '/app/plugins/',
-        'libraryDir'     => APP_PATH . '/app/library/',
-        'cacheDir'       => APP_PATH . '/app/cache/',
-        'baseUri'        => '/fms/',
-    )
+
+    'version' => '1.0',
+
+    /**
+     * if true, then we print a new line at the end of each execution
+     *
+     * If we dont print a new line,
+     * then the next command prompt will be placed directly on the left of the output
+     * and it is less readable.
+     *
+     * You can disable this behaviour if the output of your application needs to don't have a new line at end
+     */
+    'printNewLine' => true
 ));
--- www/fms/app/config/loader.php
+++ www/fms/app/config/loader.php
@@ -1,13 +1,9 @@
 <?php
 
 $loader = new \Phalcon\Loader();
-
-/**
- * We're a registering a set of directories taken from the configuration file
- */
 $loader->registerDirs(
     array(
-        $config->application->controllersDir,
-        $config->application->modelsDir
+        __DIR__ . '/../tasks'
     )
-)->register();
+);
+$loader->register();
